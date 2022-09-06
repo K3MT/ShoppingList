eb9f462372ea6ca0d96ae95aa4c297b0cd97512c
Musa Gumpu <gumpu.musa@gmail.com>
2022-09-06 19:59:27 +0200

    CORS

--- src/GVM.php
+++ src/GVM.php
@@ -5,6 +5,7 @@ class GVM
     public static function addHeaders()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            header("Access-Control-Allow-Origin: *");
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
             header("Access-Control-Allow-Headers: Authorization, Content-Type,Accept, Origin");
         exit(0);
