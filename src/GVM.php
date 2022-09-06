432cd45ff3b181c5a20a947ba71420c656d1ad69
Katlego Kungoane <92148647+KatTheGod@users.noreply.github.com>
2022-09-06 16:38:42 +0200

    Update GVM.php

--- src/GVM.php
+++ src/GVM.php
@@ -5,8 +5,8 @@ class GVM
     public static function addHeaders()
     {
         header('Access-Control-Allow-Origin: *');
-//        header('Access-Control-Allow-Methods: GET, POST');
-//        header("Access-Control-Allow-Headers: X-Requested-With");
+        header('Access-Control-Allow-Methods: GET, POST');
+        header("Access-Control-Allow-Headers: X-Requested-With");
         header('Content-Type: application/json');
     }
 
